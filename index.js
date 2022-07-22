2d3ee2ce1ea37bb7a7415b01229564c5dda1040d
Augusto Seta <alseta97@gmail.com>
2022-07-21 23:25:58 -0300

    functions added

--- index.js
+++ index.js
@@ -1,7 +1,95 @@
-let hamburgerMenuList = document.querySelector(".hamburger-menu-items")
-let cartAmount = document.querySelector(".amount");
+let hamburgerMenuList = document.querySelector(".hamburger-menu-items");
+let totalItems = document.querySelector(".totalItems");
+let totalItems2 = document.querySelector(".amount")
+let totalProducts = document.querySelector(".totalProducts");
+console.log(totalProducts);
+let cartWrapper = document.querySelector(".cartWrapper");
+console.log(cartWrapper);
 
-// //* Comportamiento del menu hamburguesa
+// INICIO: Comportamiento del Menu Hamburguesa
 const openMenu = () => {
-    hamburgerMenuList.classList.toggle("remove")
+  hamburgerMenuList.classList.toggle("remove");
+};
+
+// FIN: Comportamiento del Menu Hamburguesa
+
+
+// INICIO: Eliminar Producto
+
+const removeProduct = (product) => {
+  console.log(products);
 }
+
+
+
+// FIN: Eliminar Producto
+
+
+
+
+
+
+
+
+// INICIO: Comportamiento de los Productos
+
+let products = [];
+
+const setCount = () => {
+  let totalCount = 0;
+  for (const i in products) {
+    totalCount += products[i].count;
+  }
+
+  totalItems.innerText = totalCount.toString();
+  totalItems2.innerText = totalCount.toString();
+  return totalCount;
+};
+
+const totalPrice = () => {
+  let totalCart = 0;
+  for (const i in products) {
+    totalCart += products[i].price * products[i].count;
+  }
+  totalProducts.innerHTML = totalCart.toString();
+  return totalCart;
+};
+
+const productsList = () => {
+  cartWrapper.innerHTML = products.map((product) => {
+    return `
+            <div class="cart-items">
+                <div class="product">
+                    <p>
+                        <span class="product-name">${product.product}</span>
+                        x
+                        <span class="product-amount">${product.count}</span>
+                        : $
+                        <span class="product-price">${product.price}</span>
+                    </p>
+                    <button onclick="removeProduct(${product.product})"><i class="fa-regular fa-circle-xmark"></i></button>
+                </div>
+            </div>
+        `;
+  });
+};
+
+const addProduct = (product, price, count) => {
+  console.log(products);
+  for (const i in products) {
+    if (products[i].product === product) {
+      products[i].count++;
+      setCount();
+      totalPrice();
+      productsList();
+      return;
+    }
+  }
+
+  products.push({ product, price, count });
+  setCount();
+  totalPrice();
+  productsList();
+};
+
+// INICIO: Comportamiento de los Productos
